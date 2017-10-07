678bb469f671633639f4203087f5110229e55592
Dima Gerasimov <karlicoss@gmail.com>
2017-10-07 14:26:00 +0100

    more stuff, auth

---
+++ backup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from sys import stdout
+
+from monzo_secrets import *
+from pymonzo import MonzoAPI
+
+from kython import *
+
+# api = MonzoAPI(access_token=ACCESS_TOKEN)
+# api = MonzoAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, auth_code=AUTH_CODE)
+api = MonzoAPI() # should read token from disk
+
+[acc] = api.accounts()
+
+transactions = [t._raw_data for t in api.transactions(acc.id, reverse=False)]
+
+json_dumps(stdout, transactions)
