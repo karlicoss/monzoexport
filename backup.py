a185c9571d30d753ad451331a0797553b266efa3
Dima Gerasimov <karlicoss@gmail.com>
2017-11-14 20:07:16 +0000

    Select account

--- backup.py
+++ backup.py
@@ -10,7 +10,7 @@ from kython import *
 # api = MonzoAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, auth_code=AUTH_CODE)
 api = MonzoAPI() # should read token from disk
 
-[acc] = api.accounts()
+[acc] = [acc for acc in api.accounts() if acc.id == ACCOUNT_ID]
 
 transactions = [t._raw_data for t in api.transactions(acc.id, reverse=False)]
 
