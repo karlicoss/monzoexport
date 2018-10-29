3170c8896e00ef6336e023898c6698fed67bcb63
Dima Gerasimov <karlicoss@gmail.com>
2018-10-29 19:06:36 +0000

    moved away monzo2drebedengi

--- backup.py
+++ backup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.6
+# pip3 install pymonzo
+
 from sys import stdout
 
 from monzo_secrets import *
@@ -6,9 +8,14 @@ from pymonzo import MonzoAPI
 
 from kython import *
 
+# ugh, unclear how these two work...
 # api = MonzoAPI(access_token=ACCESS_TOKEN)
 # api = MonzoAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, auth_code=AUTH_CODE)
+
+
+# this should just read token from disk if you copy it
 api = MonzoAPI() # should read token from disk
+
 
 [acc] = [acc for acc in api.accounts() if acc.id == ACCOUNT_ID]
 
