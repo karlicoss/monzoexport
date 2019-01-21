27ee26e81832d1b3c6107c9cfb9baa0a1233e042
Dima Gerasimov <karlicoss@gmail.com>
2019-01-21 08:25:00 +0000

    fix monzo

--- backup.py
+++ backup.py
@@ -4,17 +4,29 @@
 from sys import stdout
 
 from monzo_secrets import *
+
+import pymonzo
+
+# TODO shit. how to make it generic?
+pymonzo.monzo_api.config.PYMONZO_REDIRECT_URI = REDIRECT_URI
+pymonzo.monzo_api.config.REDIRECT_URI = REDIRECT_URI
+
+
 from pymonzo import MonzoAPI
 
 from kython import *
 
-# ugh, unclear how these two work...
-# api = MonzoAPI(access_token=ACCESS_TOKEN)
+# use this if token expired
 # api = MonzoAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, auth_code=AUTH_CODE)
-
-
 # this should just read token from disk if you copy it
 api = MonzoAPI() # should read token from disk
+
+
+## upd from 210119 -- this is probably unnecessary
+# ugh, unclear how these two work...
+# api = MonzoAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, auth_code=AUTH_CODE, access_token=ACCESS_TOKEN)
+###
+
 
 
 [acc] = [acc for acc in api.accounts() if acc.id == ACCOUNT_ID]
