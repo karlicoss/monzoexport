9b9f8acb0fc76104e4ba974ec4d495828f3d2817
Dima Gerasimov <karlicoss@gmail.com>
2019-12-25 15:29:48 +0000

    add login

--- export.py
+++ export.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# pip install pymonzo
 import argparse
 import json
 from datetime import datetime, timedelta
@@ -7,12 +6,9 @@ from typing import Dict, Any
 import logging
 
 
-# TODO submodule
+# TODO submodule?
+# pip install pymonzo
 import pymonzo
-# pymonzo.monzo_api.config.PYMONZO_REDIRECT_URI = REDIRECT_URI
-# pymonzo.monzo_api.config.REDIRECT_URI = REDIRECT_URI # TODO don't need this anymore?
-
-
 from pymonzo import MonzoAPI
 
 
@@ -21,23 +17,26 @@ Json = Dict[str, Any]
 
 
 class Exporter:
-    def __init__(self, *wargs, **kwargs) -> None:
+    def __init__(self, *args, first_time=False, **kwargs) -> None:
         self.api = MonzoAPI()
+        self.first_time = first_time
 
     def _get_account_data(self, account_id: str) -> Json:
-        # transactions = [t._raw_data for t in api.transactions(acc.id, reverse=False)]
-
         # TODO add argument --first-time or something?
         # ugh. after 5 minutes past auth can only get last 90 days
         # https://docs.monzo.com/#list-transactions
         # otherwise we'd get auth error
-        since = datetime.now() - timedelta(days=90 - 1)
+        if self.first_time:
+            since = None
+        else:
+            since_dt = datetime.now() - timedelta(days=90 - 1)
+            since = f'{since_dt:%Y-%m-%dT%H:%M:%SZ}'
         transactions = self.api._get_response(
             method='get',
             endpoint='/transactions',
             params={
                 'account_id': account_id,
-                'since'     : f'{since:%Y-%m-%dT%H:%M:%SZ}',
+                'since'     : since,
             },
         ).json()['transactions']
         full_transactions = (
@@ -65,27 +64,67 @@ def get_json(**params):
     return Exporter(**params).export_json()
 
 
+def login():
+    """
+    Asking for user input here is ok; we only need to do it once
+    """
+    # see https://github.com/pawelad/pymonzo#authentication
+    redirect_uri = 'https://github.com'
+    client_id = input('client id: ')
+    client_secret = input('client secret: ')
+    # TODO patch up redicect url here?
+    auth_url = f'https://auth.monzo.com/?response_type=code&redirect_uri={redirect_uri}&client_id={client_id}'
+    print(f'Opening link to proceed with auth: {auth_url}')
+    from subprocess import run
+    try:
+        run(['xdg-open', auth_url])
+    except: # in case they not have xdg-open..
+        pass
+    auth_code = input('auth code (after browser auth): ')
+    api = MonzoAPI(
+        client_id=client_id,
+        client_secret=client_secret,
+        auth_code=auth_code,
+    )
+    print('tap in your monzo PHONE APP to allow access to the data')
+    tapped = input('press any key when tapped')
+    print("Token should be saved on disk now (you won't need to relogin anymore)")
+
+
 def main():
+    # TODO add logger configuration to export_helper?
+    # TODO autodetect logzero?
     from export_helper import setup_parser
     parser = argparse.ArgumentParser("Tool to export your personal Monzo data")
-    setup_parser(parser=parser, params=['token-path', 'account-id']) # TODO exports -- need help for each param?
+    setup_parser(parser=parser, params=['token-path']) # TODO exports -- need help for each param?
+    parser.add_argument(
+        '--first-time',
+        action='store_true',
+        help='''
+This will log you in (TODO)
+and fetch all of your transactions.
+
+After 5 minutes after login, api can only sync the last 90 days of transactions.
+See https://docs.monzo.com/#list-transactions for more information.
+''')
     args = parser.parse_args()
 
     params = args.params
     dumper = args.dumper
+    first_time = args.first_time
 
-    # use this if token expired
-    # api = MonzoAPI(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, auth_code=AUTH_CODE)
-    # this should just read token from disk
     pymonzo.monzo_api.config.TOKEN_FILE_PATH = params['token_path']
 
-    j = get_json(**params)
+    if first_time:
+        login()
+
+    j = get_json(**params, first_time=first_time)
     js = json.dumps(j, indent=1, ensure_ascii=False, sort_keys=True)
     dumper(js)
 
 
 if __name__ == '__main__':
-    # from kython.klogging import setup_logzero
-    # setup_logzero(logging.getLogger('requests_oauthlib'), level=logging.DEBUG)
+    from kython.klogging import setup_logzero
+    setup_logzero(logging.getLogger('requests_oauthlib'), level=logging.DEBUG)
     main()
 
